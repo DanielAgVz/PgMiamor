c04c11e6185bd6ad8fe2a32a3c87f738c75134cc
DanielAgVz <danielaguirre3730@gmail.com>
2024-03-27 15:55:24 -0500

    Primer27/03/2024

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+function redirigirCarta() {
+    /*window.location.href = "cartaN1.html";*/
+    alert("En proceso, aun me falta mi vida");
+  }
+  
+  function redirigirRosa() {
+    window.location.href = "Rosa.html";
+  }
+  
